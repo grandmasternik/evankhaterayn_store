24911eec957ea409cabb35a73f078c0b042c03c6
Nikki Mallory <nikkimallory13@gmail.com>
2021-06-07 16:11:23 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+// Dependecies
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+// Database Configuration
+const DATABASE_URL = 'mongodb+srv://admin:abc1234@cluster0.r1pjk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const db = mongoose.connection;
+
+// Connect to MongoDB Atlas
+mongoose.connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
+
+// Database Connection Error/Success
+// Define callback functions for various events
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// Listener
+app.listen(PORT, () => console.log(`express is listening on port: ${PORT}`));
